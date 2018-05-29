60d2e4cf14e8a6aee6975d1dae23b18ff12c1e2d
kealdor <32488803+kealdor@users.noreply.github.com>
2018-05-29 13:44:40 -0400

    Update mtga_and_tracker_launcher.py

--- scripts/mtga_and_tracker_launcher.py
+++ scripts/mtga_and_tracker_launcher.py
@@ -3,56 +3,50 @@ __date__ = "$May 28, 2018 8:27:00 PM$"
 
 from sys import platform as _platform
 import subprocess
-import psutil
 import os
+#import psutil
 
 #Set path to MTGA
-mtga_path = ''
+MTGA_Path = ''
 
 #if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
-   # linux &  # MAC OS X
-   #print ("Not Supported yet")
-    
+    # linux &  # MAC OS X
+    #print ("Not Supported yet")
+
+#For windows it assumes MTGA is installed to its default location
+
 if _platform == "win32":
-   # Windows
-   mtga_path = os.getenv("ProgramFiles") + "\\Wizards of the Coast\\MTGA\\Mtga.exe"
+    # Windows
+    MTGA_Path = os.getenv("ProgramFiles") + "\\Wizards of the Coast\\MTGA\\Mtga.exe"
    
 if _platform == "win64":
     # Windows 64-bit
-    mtga_path = os.getenv("ProgramFiles(x86)") + "\\Wizards of the Coast\\MTGA\\Mtga.exe"
+    MTGA_Path = os.getenv("ProgramFiles(x86)") + "\\Wizards of the Coast\\MTGA\\Mtga.exe"
 
 #Set path to MTGA Tracker
-mtga_tracker_path = ''
+MTGA_Tracker_Path = ''
 
 #if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
-   # linux &  # MAC OS X
-   #print ("Not Supported yet")
+    # linux &  # MAC OS X
+    #print ("Not Supported yet")
     
 if _platform == "win32" or _platform == "win64":
-   # Windows
-   mtga_tracker_path = os.getenv("USERPROFILE") + "\\AppData\\Local\\mtgatracker\\MTGATracker.exe"
+    # Windows
+    MTGA_Tracker_Path = os.getenv("USERPROFILE") + "\\AppData\\Local\\mtgatracker\\MTGATracker.exe"
    
 #Run The Exe's
-os.system(mtga_path)
-os.system(mtga_tracker_path)
+os.system(MTGA_Path)
+os.system(MTGA_Tracker_Path)
 
 #Close The Exe's when needed
-n = 0 # number of instances of the program running 
-
+#Shards fix to work alot better 5/29/2018 at 11:30am
 prog = [line.split() for line in subprocess.check_output("tasklist").splitlines()]
-prog.pop(e) for e in range(3)]
+del prog[0]
+prog = [i[0].decode("utf-8") for i in prog]
 
-for task in prog:
-    if task[0]=="MTGA.exe":
-      n = 1
-      
-    if n > 0:
-      return True
-   
-    if n == 0:
-         if _platform == "win32" or _platform == "win64":
-            os.system("TASKKILL /F /IM MTGATracker.exe")
-         
-         #if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
+if not 'MTGA.exe' in prog:
+    if _platform == "win32" or _platform == "win64":
+        os.system("TASKKILL /F /IM MTGATracker.exe")
+    #if _platform == "linux" or _platform == "linux2" or _platform == "darwin":
             #for pid in (process.pid for process in psutil.process_iter() if process.name()=="MTGATracker.exe"):
-               #os.kill(pid)
+                #os.kill(pid)
